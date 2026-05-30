adff49cb4b67cb7b2941af07b2817c00ff6578a2
PaoPei01 <paopei2706@gmail.com>
2026-05-30 18:51:50 +0700

    Fix event form deadline migration

--- supabase/migrations/202605300001_extend_parent_orientation_staff_application_close.sql
+++ supabase/migrations/202605300001_extend_parent_orientation_staff_application_close.sql
@@ -1,6 +1,5 @@
 update public.event_forms ef
-set closes_at = '2026-05-31 23:59:59+07'::timestamptz,
-    updated_at = now()
+set closes_at = '2026-05-31 23:59:59+07'::timestamptz
 from public.events e
 where ef.event_id = e.id
   and e.slug = 'parent-orientation-staff-2569'
